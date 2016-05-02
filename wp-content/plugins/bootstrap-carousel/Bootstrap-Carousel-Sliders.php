d8c11fbe7a8bf621eeb6e47077fa5b0ba438bac1
SoLoGHoST <solomon_closson@yahoo.com>
2016-05-02 05:28:15 -0400

    admin page settings added.

--- wp-content/plugins/bootstrap-carousel/Bootstrap-Carousel-Sliders.php
+++ wp-content/plugins/bootstrap-carousel/Bootstrap-Carousel-Sliders.php
@@ -2,12 +2,48 @@
 /**
  * Plugin Name: Bootstrap Carousel Sliders
  * Description: Provides ability for Specific Bootstrap Carousel Sliders on post types, archives, tags, and/or taxonomies.  Pull images in from ACF field(s), and/or media attachments.  Adds filters for editing these images manually.
- * Plugin URI: http://gelest.com 
- * Author:      Think it First
+ * Plugin URI: http://thinkitfirst.com
+ * Author:     Solomon Closson
  * Author URI: http://thinkitfirst.com
- * Version:     0.0.1
+ * Version:     0.1.0
  */
 
+load_plugin_textdomain('bssliders', false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+// plugin_dir_url( __FILE__ ).'icons/my_icon.png'
+
+if (is_admin())
+{
+	add_action('admin_menu', 'bs_carousel_menu');
+	add_action('admin_init', 'bs_register_settings');
+}
+
+function bs_carousel_menu() {
+    add_menu_page (
+        'Bootstrap Carousel Sliders Settings',
+        'BS Carousel Sliders',
+        'manage_options',
+        'bootstrap-carousel/admin.php',
+        '',
+        'dashicons-format-gallery',
+        '10'
+    );
+}
+
+function bs_register_settings() {
+	register_setting('bs-settings-group', 'bs_posttypes');
+	register_setting('bs-settings-group', 'bs_pages_all');
+	register_setting('bs-settings-group', 'bs_pages');
+	register_setting('bs-settings-group', 'bs_archives');
+	register_setting('bs-settings-group', 'bs_taxonomies_all');
+	register_setting('bs-settings-group', 'bs_taxonomies');
+	register_setting('bs-settings-group', 'bs_featured_image');
+
+	if (class_exists('acf'))
+		register_setting('bs-settings-group', 'bs_acf_field_slug');
+}
+
+// Make sure to include Bootstrap, js, and/or css plugin files with wp_enqueue_script and wp_enqueue_styles respectively!
 
 
 
@@ -17,13 +53,13 @@
 
 /*
 array(
-	'post_types' => array('gelest_application', 'team_member', 'press_release'),
+	'post_types' => array('gelest_application', 'team_member', 'press_release'), // post types
 	'taxonomies' => array(
-		'press_release' => array('press_release_years')
+		'press_release' => array('press_release_years') // post types and taxonomies/terms
 	),
-	'pages' => array('all'),
-	'archives' => array('gelest_application'),
-	'use_featured_image' => true
+	'pages' => array('all'), // all page slugs
+	'archives' => array('gelest_application'), post types that have archives ability
+	'use_featured_image' => true // if use_featured_image, checkbox setting!
 );
 */
 
